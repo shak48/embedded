ea7daa3ab074e1b1dea1faa31dcc813f891353b7
shak48 <rumie.kabir@gmail.com>
2023-09-04 20:50:02 -0400

    Added tests for int and timer. Added Nucleo project.

--- Arduino/Micro/TestInterrupts/src/main.cpp
+++ Arduino/Micro/TestInterrupts/src/main.cpp
@@ -1,68 +1,87 @@
 #include <Arduino.h>
-#include "TimerInterrupt.h"
-#include "ISR_Timer.h" 
+//#include "TimerInterrupt.h"
+//#include "ISR_Timer.h" 
 
 
 #define DI1 7
 #define DO1 8
 
 
-volatile byte flag=HIGH;
-void int1Handler(void);
+int COMP_MATCH1,COMP_MATCH2;
 
-void setup() {
-  pinMode(13, OUTPUT);    // sets the digital pin 13 as output
+volatile boolean DI1_STATE=HIGH;
+void ISR1(void);
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT);    
+
   pinMode(DI1, INPUT);    
   digitalWrite(DI1, HIGH); // int pullup
+  
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  PCICR |= B00000100;
+
+  PCMSK0 |= B10000100;
+
+  COMP_MATCH1=15000;
+  COMP_MATCH2=60000;
+  TCNT1 = COMP_MATCH2;
+
+  TCCR1B |= (1<<CS12);
+  //TCCR1B |= (1<<CS11);
+  //TCCR1B |= (1<<CS10);
+
+  TIMSK1 |= (1<<OCIE1A);
+
+  attachInterrupt(digitalPinToInterrupt(DI1),ISR1,FALLING);
+  interrupts();
 
   Serial.begin(9600);
   Serial1.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(DI1),int1Handler,CHANGE);
-  interrupts();
 
-
-  
+ 
 }
 
 void loop() {
-  digitalWrite(13, HIGH); // sets the digital pin 13 on
+/*   
+digitalWrite(13, HIGH); // sets the digital pin 13 on
   digitalWrite(DO1, HIGH); // sets the digital pin 13 on
 
   delay(200);            // waits for a second
   digitalWrite(13, LOW);  // sets the digital pin 13 off
   digitalWrite(DO1, HIGH); // sets the digital pin 13 on
 
-  delay(200);            // waits for a second
-  Serial.println("OK! From VS CODE");
-  Serial1.println("OK! From VS Serial1");
-  Serial1.println(digitalRead(DI1));
+  delay(200);            // waits for a second 
+  */
+
+ if(DI1_STATE==LOW)
+ {
+  DI1_STATE = HIGH;
+  Serial.println("DI1 STATE LOW###############################################################################");
+ }
+
+
+  //Serial.println("OK");
 
 }
 
-void int1Handler(void)
+ISR(TIMER1_COMPA_vect)
 {
-  if(digitalRead(DI1)==HIGH)
-  {
-    flag = HIGH;
-  }
-  else
-  {
-    flag=LOW;
-  }
+  TCNT1 = COMP_MATCH1;
+    digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN)^1); 
 }
 
-void pollDIFlags(int pinNumber){
-  static byte prevFlagState = flag;
-if(digitalRead(digitalRead(DI1) != prevFlagState))
-{
-   Serial1.println("DI1 Updated! ");
-  Serial1.println(digitalRead(DI1));
 
-  Serial1.println(digitalRead(DI1));
-
+void ISR1(void){
+DI1_STATE=LOW;
 }
 
-}
+
+
+
 
 
 
